11e810208e9a06cd2f4604c798eb40adf7347146
Hitalo Alves <hitalo.ralves@outlook.com>
2021-08-15 14:50:27 -0300

    Finalizando estilo do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import YouTubeIcon from '@material-ui/icons/YouTube';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 import './index.css';
@@ -13,11 +11,9 @@ const Footer = () => {
             <footer>
                 <div className="footer-container">
                     <div className="footer-social-links">
-                        <a href="/#"> <FacebookIcon /> </a>
-                        <a href="/#"> <InstagramIcon /> </a>
-                        <a href="/#"> <LinkedInIcon /> </a>
-                        <a href="/#"> <TwitterIcon /> </a>
-                        <a href="/#">  <YouTubeIcon /> </a>
+                        <a href="https://www.facebook.com/hitalorodrigo.alves" className="social-link-facebook"> <FacebookIcon /> </a>
+                        <a href="https://www.instagram.com/hitaloalvees/" className="social-link-instagram"> <InstagramIcon /> </a>
+                        <a href="https://www.linkedin.com/in/hitalo-alves/" className="social-link-linkedin"> <LinkedInIcon /> </a>
                     </div>
                     <ul className="footer-links">
                         <li className="footer-link">
@@ -67,7 +63,7 @@ const Footer = () => {
 
                     <div className="footer-copyright">
                         <span>
-                        © 2021... Alvesflix, Inc. Inspirado © Netflix, Inc -     
+                        © 2021... Alvesflix. Inspirado em © Netflix, Inc.    
                         </span>
                         <span>
                             Feito com <FavoriteIcon style={{color:'red'}} /> por Hitalo.
